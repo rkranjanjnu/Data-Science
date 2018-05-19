d2a7398813d931c4e801701d1889c78a16e9aeb3
Ranjeet K. Ranjan <ranjeetranjan@Ranjeets-Air.mshome.net>
2018-05-20 00:14:31 +0530

    my first class based program

---
+++ data.py
@@ -0,0 +1,11 @@
+class data:
+	def __init__(self, a, b):
+		self.a = a
+                self.b = b
+        def sum(self):
+		c = self.a+self.b
+                return c
+
+d = data(5, 10)
+print(d.sum())
+		
